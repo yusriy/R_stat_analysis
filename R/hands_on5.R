b557bde578d77ae3d3be8c4e4eff5f5e66f77c15
yusriy <yusriyp@gmail.com>
2016-04-10 12:55:06 +0800

    Modified dotplot to sort the data first; added PDF of effective graphs

--- R/hands_on5.R
+++ R/hands_on5.R
@@ -1,4 +1,10 @@
 # Hands-on 5
 # From Hands-on 4, please click 'clear all' to reset plot window.
 ho5_data <- read.csv(file = 'data/ho5_data.csv', header = TRUE)
-dotchart(ho5_data$Zn..mg.L.,xlab='Zn (mg/L)',pch=19)
+# Sort by concentration
+x <- ho5_data[order(ho5_data$Zn..mg.L.),]
+# Sorted order of Zn concentration
+dotchart(x,pch=19,xlab='Zn (mg/L)',cex = 1)
+dotchart(ho5_data$Zn..mg.L.,pch=19,xlab='Zn (mg/L)',cex = 1)
+
+
