23e188944d80b9c0aee6f5a8c1aa24f0a8b58f2f
yusriy <yusriyp@gmail.com>
2016-04-10 10:19:31 +0800

    Added pareto chart

--- R/hands_on2.R
+++ R/hands_on2.R
@@ -1,4 +1,6 @@
 # Hands-on 2
+install.packages('qcc') # For Pareto chart
+library(qcc)
 
 ho2_data <- read.csv(file = "data/ho2_data.csv", header = TRUE)
 names(ho2_data)[1] <- 'metal'
@@ -7,3 +9,8 @@ names(ho2_data)[2] <- 'concentration'
 barplot(ho2_data$concentration,xlab="Heavy metal",ylab="Concentration in water (mg/L)",
         names.arg=ho2_data$metal,ylim=c(0,0.32),col='lightblue')
 box()
+
+# Optional, Pareto chart
+concentration <- ho2_data$concentration
+names(concentration) <- ho2_data$metal
+pareto.chart(concentration, ylab = 'concentration')
