521dee413a6ded011727a5f57a4a3a5908074875
yusriy <yusriyp@gmail.com>
2017-08-20 13:08:24 +0800

    Added code method to export plots.

--- R/hands_on3.R
+++ R/hands_on3.R
@@ -11,3 +11,24 @@ legend("bottomright",c('Control','GCPe'),lty=c(1,1),pch=c(1,19),col=c('black','b
 axis(1,at=seq(0,200,by=10))
 axis(2,at=seq(0,30,by=1))
 box()
+
+# To export using code (recommended)
+
+jpeg('figs/ho3_data_code.jpeg', res = 360, height = 8, width = 8, units = 'cm')
+
+par(mar = c(3.1,3.1,1.1,1.1), mgp = c(3.0,0.3,0), family = 'Times')
+plot(ho3_data$Time..min.,ho3_data$Control,type='o',xlab='',ylab='',
+     axes=FALSE)
+mtext(side = 1, 'Time (min)', line = 1.8, cex = 1.2)
+mtext(side = 2, 'Hydrolysis (%)', line = 1.8, cex = 1.2)
+
+lines(ho3_data$Time..min.,ho3_data$GCPe,type='o',pch=19,col='blue')
+
+legend("bottomright",c('Control','GCPe'),lty=c(1,1),pch=c(1,19),col=c('black','blue'))
+
+axis(1,at=seq(0,200,by=10),tck=-0.02)
+axis(2,at=seq(0,30,by=1),tck=-0.02)
+
+box()
+
+dev.off()
